005c86118283ab9bcc956f33b351fdcb118ac353
Rogerio Eduardo <161332525+onilotap@users.noreply.github.com>
2024-04-15 12:12:55 -0300

    Update projetinho.c

--- projetinho.c
+++ projetinho.c
@@ -1,93 +1,51 @@
-/*
- ============================================================================
- Name        : projetinho.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-int main(void) {
-	int escolha;
+#include <time.h>
 
-	printf("digite um numero de 1 a 5: ");
-	fflush(stdout);
-	scanf("%d", &escolha);
-
-	if (escolha == 1) {
-		printf("voce escolheu o numero 1.\n");
-	} else if (escolha == 2) {
-		printf("voce escolheu o numero 2.\n");
-	} else if (escolha == 3) {
-		printf("voce escolheu o numero 3.\n");
-	} else if (escolha == 4) {
-		printf("voce escolheu o numero 4.\n");
-	} else if (escolha == 5) {
-		printf("voce escolheu o numero 5.\n");
-	} else {
-		printf("opção inválida.\n");
-	}
-
-	// alternativa mais enxuta
-
-	switch (escolha) {
-	case 1:
-		printf("voce escolheu o numero 1.\n");
-		break;
-	case 2:
-		printf("voce escolheu o numero 2.\n");
-		break;
-	case 3:
-		printf("voce escolheu o numero 3.\n");
-		break;
-	case 4:
-		printf("voce escolheu o numero 4.\n");
-		break;
-	case 5:
-		printf("voce escolheu o numero 5.\n");
-		break;
-
-	default:
-		printf("opção invalida.\n");
-	}
-
-	int x, y, result, cont;
-	cont = 1;
-
-	while (cont <= 3) {
-		printf("entre com os dois numeros para serem somados: \n");
-		scanf("%d %d", &x, &y);
-
-		result = x + y;
-
-		printf("o resultado da soma é: %d\n", result);
-
-		cont = cont + 1;
-	}
-
-	cont = 1;
-	do {
-		printf("entre com os dois numeros para serem somados: \n");
-		fflush(stdout);
-		scanf("%d %d", &x, &y);
-
-		result = x + y;
-		printf("o valor da soma e %d \n", result);
-		cont++;
-	} while (cont <= 3);
-
-	for (cont = 1; cont <= 3; cont++) {
-		printf("entre com os dois numeros para serem somados: \n");
-		fflush(stdout);
-		scanf("%d %d", &x, &y);
-
-		result = x + y;
-
-		printf("o resultado da soma e: %d\n", result);
-	}
-	return 0;
+int random_number(int min, int max) {
+  return min + rand() % (max - min + 1);
 }
+
+int main () {
+  srand(time(NULL));
+
+  int portaPremiada, portaEscolhida, portaAbertaPeloApresentador, novaEscolha;
+
+  portaPremiada = random_number(1,3);
+
+  printf("Bem-vindo ao jogo Monty Hall!.\n");
+
+  printf("atrás de uma das três portas há um prêmio, escolha uma porta.\n:");
+
+  printf("escolha uma porta (1, 2 ou 3):");
+
+  scanf("%d", &portaEscolhida);
+
+  do {
+    portaAbertaPeloApresentador = random_number(1,3);
+  }
+    while(portaAbertaPeloApresentador == portaPremiada || portaAbertaPeloApresentador == portaEscolhida);
+
+  printf("o apresentador abriu a porta %d, que não contém o prêmio.\n", portaAbertaPeloApresentador);
+
+  printf("você deseja trocar de porta? sim(1) ou não(2):");
+
+  scanf("%d", &novaEscolha);
+
+  if(novaEscolha==1) {
+    for (int i = 1; i <= 3; i++){
+      if(i != portaEscolhida && i != portaAbertaPeloApresentador) {
+        portaEscolhida = i;
+        break;
+      }
+    }
+  f (portaEscolhida == portaPremiada) {
+    printf("parabéns! você ganhou o prêmio!\n");
+  } else{
+
+    printf("infelizmente, você perdeu, o premio estava atrás da porta %d.\n", portaPremiada);
+} 
+  return 0;
+}
+
 
